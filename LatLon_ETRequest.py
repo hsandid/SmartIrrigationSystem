97f3efb3a3925f3ab278e650785bd2a464d98f7c
hsandid <36267944+hsandid@users.noreply.github.com>
2020-04-26 00:23:10 +0300

    Add files via upload

---
+++ LatLon_ETRequest.py
@@ -0,0 +1,47 @@
+#Import the RPi.GPIO library
+import RPi.GPIO as GPIO
+#Import the time library
+import time
+#Import the requests library 
+import requests 
+#Import the JSON library
+import json
+
+#Initializing GPIO ports
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
+GPIO.output(2,GPIO.LOW)
+GPIO.output(3,GPIO.LOW)
+
+  
+#Defining the API-endpoint  
+API_ENDPOINT = "https://promising-keep-261206.appspot.com/tiff/coordinates_to_et?latitude=33.244646&longitude=35.386042"
+
+#Defining Headers
+headers = {'Content-Type' : 'application/json'}
+  
+#Sending Get request and saving response as response object 
+r = requests.get(url = API_ENDPOINT, headers=headers) 
+  
+#Extracting text response 
+textresponse = r.json()
+
+#Check if the response contains a valid ET value and act accordingly
+if textresponse.get('et_value') is None:
+    print("No ET value for this field!")
+    GPIO.output(2,GPIO.HIGH)
+    time.sleep(3)
+
+else :
+    Et_Value = float(textresponse.get('et_value'))
+    Et_Value = int(Et_Value)
+    print(Et_Value)
+
+    for x in range(5):
+        GPIO.output(3,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(3,GPIO.LOW)
+        time.sleep(0.5)
+    
