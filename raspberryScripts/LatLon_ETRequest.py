614ffdd2a66fbc0bd910c21e98753a5624b584c3
hsandid <36267944+hsandid@users.noreply.github.com>
2020-05-02 19:22:43 +0300

    Update LatLon_ETRequest.py

--- raspberryScripts/LatLon_ETRequest.py
+++ raspberryScripts/LatLon_ETRequest.py
@@ -1,13 +1,19 @@
 #Import the RPi.GPIO library
 import RPi.GPIO as GPIO
+
 #Import the time library
 import time
+
 #Import the requests library 
 import requests 
+
 #Import the JSON library
 import json
 
 #Initializing GPIO ports
+#GPIO Pin 2 is assigned as output (Red LED on circuit)
+#GPIO Pin 3 is assigned as output (Green LED on circuit)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(2,GPIO.OUT)
@@ -17,7 +23,7 @@ GPIO.output(3,GPIO.LOW)
 
   
 #Defining the API-endpoint  
-API_ENDPOINT = "https://promising-keep-261206.appspot.com/tiff/coordinates_to_et?latitude=33.244646&longitude=35.386042"
+API_ENDPOINT = "<BACKEND_URL>/tiff/coordinates_to_et?latitude=33.244646&longitude=35.386042"
 
 #Defining Headers
 headers = {'Content-Type' : 'application/json'}
@@ -28,7 +34,10 @@ r = requests.get(url = API_ENDPOINT, headers=headers)
 #Extracting text response 
 textresponse = r.json()
 
-#Check if the response contains a valid ET value and act accordingly
+#Check if the response contains a valid ET value
+#If the ET value is valid, flash the Green LED according to the ET value
+#If the ET value is invalid, flash the Red LED once
+
 if textresponse.get('et_value') is None:
     print("No ET value for this field!")
     GPIO.output(2,GPIO.HIGH)
